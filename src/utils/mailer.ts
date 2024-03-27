542097130db81be6342e095bef9d1b38184ccb09
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2024-03-27 09:50:57 +0100

    feat: switch mailer host

--- src/utils/mailer.ts
+++ src/utils/mailer.ts
@@ -21,19 +21,6 @@ class EmailService {
     }
 
     newTransport() {
-        if(process.env.NODE_ENV === 'production') {
-            return nodemailer.createTransport({
-                host: process.env.MAIL_HOST,
-                port: process.env.MAIL_PORT,
-                secure: false,
-                requireTLS: true,
-                auth: {
-                    user: process.env.MAIL_USER,
-                    pass: process.env.MAIL_PASS,
-                },
-            } as nodemailer.TransportOptions);
-        }
-
         return nodemailer.createTransport({
             host: process.env.MAIL_HOST,
             port: process.env.MAIL_PORT,
